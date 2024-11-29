1c8c7941aed535ce9fee19798f1bc19851145d42
Hari Muhlia <harimuhlia@gmail.com>
2024-11-29 11:41:03 +0700

    Menambah halaman list peserta

--- resources/views/tampilan/sidebar.blade.php
+++ resources/views/tampilan/sidebar.blade.php
@@ -63,10 +63,24 @@
               <li class="nav-item">
                 <a href="{{ route('listpeserta.index')}}" class="nav-link {{ (request()->is('listpeserta*')) ? 'active' : '' }}">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Data Peserta</p>
+                  <p>List Peserta</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Verifikasi Peserta</p>
                 </a>
               </li>
             </ul>
+          </li>
+          <li class="nav-item">
+            <a href="{{ route('home')}}" class="nav-link {{ (request()->is('dashboard*')) ? 'active' : '' }}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Pengumuman
+              </p>
+            </a>
           </li>
           <li class="nav-header">PENGATURAN</li>
           <li class="nav-item">
